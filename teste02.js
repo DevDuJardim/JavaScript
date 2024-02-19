6d0f44ca1a53af6a921bbece4d697375074d2069
diego do <>
2024-02-19 20:22:07 -0300

    vai

--- teste02.js
+++
@@ -1,5 +0,0 @@
-var s = "test";                                       // Começa com um valor de string.
-s.len = 4;                                          // Configura uma propriedade nele.
-var t = s.len;
-
-console.log(t)
